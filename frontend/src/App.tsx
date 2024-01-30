0e54cf47414896c5ba5f1836c8a805f128de8767
Alvaro-78 <alvaro78an@gmail.com>
2024-01-30 16:18:40 +0100

    Added routes to app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import OperationList from './components/OperationList';
+import AddOperationForm from './components/AddOperationForm';
+
+function App() {
+	return (
+		<>
+			<Navbar />
+			<Routes>
+				<Route path="operations" element={<OperationList />} />
+				<Route path="create-operations" element={<AddOperationForm />} />
+			</Routes>
+		</>
+	);
+}
+
+export default App;
