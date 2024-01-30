fb32c3f3475920fafe0b89b14dfa03f3b5cf3cdb
Alvaro-78 <alvaro78an@gmail.com>
2024-01-30 16:57:08 +0100

    Added alert if soemthign went worng

--- frontend/src/components/AddOperationForm.tsx
+++ frontend/src/components/AddOperationForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, FormEvent, useEffect } from 'react';
+import React, { useState, ChangeEvent, FormEvent } from 'react';
 import { IOperation } from '../interfaces/Operation';
 import axios from 'axios';
 
@@ -30,9 +30,16 @@ const AddOperationForm = () => {
 				'http://localhost:3001/operations',
 				operation
 			);
-			console.log(response, 'response');
 		} catch (error) {
-			console.log('Somethings went wrong !!!', error);
+			if (axios.isAxiosError(error)) {
+				// Manejo de errores específicos de Axios.
+				console.error('Error de Axios:', error.message);
+				alert('Error al hacer la solicitud.');
+			} else {
+				// Manejo de errores inesperados.
+				console.error('Error inesperado:', error);
+				alert('Error inesperado al hacer la solicitud.');
+			}
 		}
 
 		setOperation({
@@ -46,96 +53,103 @@ const AddOperationForm = () => {
 	};
 
 	return (
-		<form onSubmit={handleSubmit} className="container-sm mt-5">
-			<div className="mb-3">
-				<label htmlFor="validationTooltip01" className="form-label">
-					Nombre de la Comercializadora
-				</label>
-				<input
-					name="marketerId"
-					type="text"
-					className="form-control"
-					id="providerName"
-					value={operation.marketerId}
-					onChange={handleChange}
-					required
-				/>
+		<form onSubmit={handleSubmit} className="container center mt-5">
+			<div className="row">
+				<div className="col-md-4 mb-3">
+					<label htmlFor="validationTooltip01" className="form-label">
+						Id de la Comercializadora
+					</label>
+					<input
+						name="marketerId"
+						type="text"
+						className="form-control"
+						id="providerName"
+						value={operation.marketerId}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+				<div className="col-md-4 mb-3">
+					<label htmlFor="tradingPartnerName" className="form-label">
+						Id de la Comercializadora a Comerciar
+					</label>
+					<input
+						name="clientId"
+						type="text"
+						className="form-control"
+						id="tradingPartnerName"
+						value={operation.clientId}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+				<div className="col-md-4 mb-3">
+					<label htmlFor="type" className="form-label">
+						Tipo de transacción
+					</label>
+					<select
+						name="type"
+						className="form-select"
+						id="type"
+						value={operation.type}
+						onChange={handleChange}>
+						<option value="Compra">Compra</option>
+						<option value="Venta">Venta</option>
+					</select>
+				</div>
 			</div>
-			<div className="mb-3">
-				<label htmlFor="tradingPartnerName" className="form-label">
-					Nombre de la Comercializadora a Comerciar
-				</label>
-				<input
-					name="clientId"
-					type="text"
-					className="form-control"
-					id="tradingPartnerName"
-					value={operation.clientId}
-					onChange={handleChange}
-					required
-				/>
+
+			<div className="row">
+				<div className="col-md-4 mb-3">
+					<label htmlFor="gasAmount" className="form-label">
+						Cantidad de Gas
+					</label>
+					<input
+						name="amount"
+						type="number"
+						min="0"
+						className="form-control"
+						id="gasAmount"
+						value={operation.amount}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+				<div className="col-md-4 mb-3">
+					<label htmlFor="price" className="form-label">
+						Precio
+					</label>
+					<input
+						name="price"
+						type="number"
+						min="0"
+						className="form-control"
+						id="price"
+						value={operation.price}
+						onChange={handleChange}
+						required
+					/>
+				</div>
+				<div className="col-md-4 mb-3">
+					<label htmlFor="currenty_currency" className="form-label">
+						Moneda
+					</label>
+					<select
+						name="currenty_currency"
+						className="form-select"
+						id="currency"
+						value={operation.currenty_currency}
+						onChange={handleChange}>
+						<option value="EUR">EUR</option>
+						<option value="USD">USD</option>
+					</select>
+				</div>
 			</div>
-			<div className="mb-3">
-				<label htmlFor="type" className="form-label">
-					Tipo
-				</label>
-				<select
-					name="type"
-					className="form-select"
-					id="type"
-					value={operation.type}
-					onChange={handleChange}>
-					<option value="Compra">Compra</option>
-					<option value="Venta">Venta</option>
-				</select>
+			<div id="button" className="d-flex justify-content-center">
+				<button type="submit" className="btn btn-primary ">
+					Añadir Operación
+				</button>
 			</div>
-			<div className="mb-3">
-				<label htmlFor="gasAmount" className="form-label">
-					Cantidad de Gas
-				</label>
-				<input
-					name="amount"
-					type="number"
-					min="0"
-					className="form-control"
-					id="gasAmount"
-					value={operation.amount}
-					onChange={handleChange}
-					required
-				/>
-			</div>
-			<div className="mb-3">
-				<label htmlFor="price" className="form-label">
-					Precio
-				</label>
-				<input
-					name="price"
-					type="number"
-					min="0"
-					className="form-control"
-					id="price"
-					value={operation.price}
-					onChange={handleChange}
-					required
-				/>
-			</div>
-			<div className="mb-3">
-				<label htmlFor="currenty_currency" className="form-label">
-					Moneda
-				</label>
-				<select
-					name="currenty_currency"
-					className="form-select"
-					id="currency"
-					value={operation.currenty_currency}
-					onChange={handleChange}>
-					<option value="EUR">EUR</option>
-					<option value="USD">USD</option>
-				</select>
-			</div>
-			<button type="submit" className="btn btn-primary">
-				Añadir Operación
-			</button>
 		</form>
 	);
 };
